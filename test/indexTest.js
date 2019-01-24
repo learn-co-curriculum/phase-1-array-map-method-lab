a4fa4b8cf07f0c5d6a0271008e60246113575293
Kellye Greene <kellyecodes@gmail.com>
2019-01-24 15:17:40 -0500

    Updates tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,19 +7,7 @@ describe('index.js', function () {
     });
 
     it('returns an array with title case tutorial names', function () {
-      const tutorials = [
-        { name: 'What does the this keyword mean?', duration: 80 },
-        { name: 'What is the Constructor OO pattern?', duration: 40 },
-        { name: 'Implementing Blockchain Web API', duration: 100 },
-        { name: 'The Test Driven Development Workflow', duration: 140 },
-        { name: 'What is NaN and how can we check for it', duration: 50 },
-        { name: 'What is the difference between stopPropagation and preventDefault?', duration: 90 },
-        { name: 'Immutable State and Pure Functions', duration: 75 },
-        { name: 'What is the difference between == and ===?', duration: 30 },
-        { name: 'What is the difference between event capturing and bubbling?', duration: 60 },
-        { name: 'What is JSONP?', duration: 25 },
-      ];
-      expect(titleCased(tutorials)).to.have.all.members(
+      expect(titleCased).to.have.all.members(
         [
           "What Does The This Keyword Mean?",
           "What Is The Contutorialuctor OO Pattern?",
