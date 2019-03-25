d523b85be84aef53a6f803e33e3b101537989268
Steven G. Harms <steven.harms@flatironschool.com>
2019-03-25 11:15:06 -0400

    Remove tutorials constant

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
+
+const titleCased = () => {
+  return tutorials
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,16 +1,3 @@
-const tutorials = [
-  'what does the this keyword mean?', 
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
 describe('index.js', function () {
   describe('titleCased()', function () {
     it('returns an array with title case tutorial names', function () {
