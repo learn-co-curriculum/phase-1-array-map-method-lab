dba314b3d3c1dfa34a6d29fd68e13e1e11bd1e5f
Ian Hollander <ianholla@gmail.com>
2022-01-06 11:02:35 -0500

    Remove useless param and fix formatting

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = (input) => {
-  return tutorials.map( line => {
-    const tokens = line.split(' ');
-    const capitalizedTokens =
-      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) );
-    const response = capitalizedTokens.join(' ');
+const titleCased = () => {
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
     return response;
   });
-}
+};
